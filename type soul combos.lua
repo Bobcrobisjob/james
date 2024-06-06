681c7b770ffca2db14f08a71f3334c6320a475bb
Bobcrobisjob <76071718+Bobcrobisjob@users.noreply.github.com>
2024-06-06 12:55:41 -0400

    Update type soul combos.lua

--- type soul combos.lua
+++ type soul combos.lua
@@ -2,48 +2,10 @@ loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-MoreUNC-131
 
 local VirtualInputManager = Instance.new('VirtualInputManager')
 
---[[
 function Target()
-	return game.Players.OatherThegoather.Character
+	return game.Players.noeyel0905.Character
 end
-]]--
 
-function Target()
-	local target = nil
-	local distance = math.huge
-
-	for i,v in next, workspace.Entities:GetChildren() do
-		if game.Players:FindFirstChild(v.Name) then
-			if game.Players.LocalPlayer:GetAttribute("Party") and game.Players[v.Name]:GetAttribute("Party") then
-				if v and v.Name~=game.Players.LocalPlayer.Name and v:FindFirstChild("HumanoidRootPart") and math.abs(v:FindFirstChild("HumanoidRootPart").Position.Y - game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Position.Y) <= math.huge and v:FindFirstChildOfClass("Humanoid") and v:FindFirstChildOfClass("Humanoid").Health>0 and v:GetAttribute("CurrentState") ~= "Unconscious" and game.Players.LocalPlayer:GetAttribute("Party") ~= game.Players[v.Name]:GetAttribute("Party") then
-					local plrdist = game.Players.LocalPlayer:DistanceFromCharacter(v:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p)
-					if plrdist < distance then
-						target = v
-						distance = plrdist
-					end
-				end
-			else
-				if v and v.Name~=game.Players.LocalPlayer.Name and v:FindFirstChild("HumanoidRootPart") and math.abs(v:FindFirstChild("HumanoidRootPart").Position.Y - game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Position.Y) <= math.huge and v:FindFirstChildOfClass("Humanoid") and v:FindFirstChildOfClass("Humanoid").Health>0 and v:GetAttribute("CurrentState") ~= "Unconscious" then
-					local plrdist = game.Players.LocalPlayer:DistanceFromCharacter(v:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p)
-					if plrdist < distance then
-						target = v
-						distance = plrdist
-					end
-				end
-			end
-		else
-			if v and v.Name~=game.Players.LocalPlayer.Name and not v.Name:lower():find(("FlashClone"):lower()) and not v.Name:lower():find(("Wolf"):lower()) and v:FindFirstChild("HumanoidRootPart") and math.abs(v:FindFirstChild("HumanoidRootPart").Position.Y - game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Position.Y) <= math.huge and v:FindFirstChildOfClass("Humanoid") and v:FindFirstChildOfClass("Humanoid").Health>0 and v:GetAttribute("CurrentState") ~= "Unconscious" then
-				local plrdist = game.Players.LocalPlayer:DistanceFromCharacter(v:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p)
-				if plrdist < distance then
-					target = v
-					distance = plrdist
-				end
-			end
-		end
-	end
-
-	return target
-end
 
 local function XZ(V)
 	return Vector3.new(V.X, 0, V.Z)
