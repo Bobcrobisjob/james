913719991fa4dfdfe637103a4b18043061684879
Bobcrobisjob <76071718+Bobcrobisjob@users.noreply.github.com>
2024-10-27 20:21:23 -0400

    Update type soul combos.lua

--- type soul combos.lua
+++ type soul combos.lua
@@ -1,1 +1,51 @@
 
+task.wait(3)
+local queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+Players.LocalPlayer.OnTeleport:Connect(function(State)
+	queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
+end)
+local b = settings();
+
+local debounce = true
+
+local runLoop
+
+runLoop = game:GetService("RunService").RenderStepped:Connect(function(deltaTime) 
+	pcall(function()
+
+
+			if debounce == true then
+				--game.Players.LocalPlayer.Character:FindFirstChild("Basketball")
+				if game.Players.LocalPlayer.Character.HumanoidRootPart.Basketball.Part1 ~= nil then
+                    delay(1.5, function()
+						if game.Players.LocalPlayer.Character.HumanoidRootPart.Basketball.Part1 ~= nil then
+							b.Network.IncomingReplicationLag = 0.225;
+						end
+					end)
+                else
+					b.Network.IncomingReplicationLag = 0;
+                end
+			end
+
+	end)
+end)
+
+game.UserInputService.InputBegan:connect(function(input)
+	if input.KeyCode == Enum.KeyCode.End then
+		runLoop:Disconnect()
+        b.Network.IncomingReplicationLag = 0;
+	end
+end)
+
+game.UserInputService.InputBegan:connect(function(input)
+	if input.KeyCode == Enum.KeyCode.Zero then
+		debounce = false
+        b.Network.IncomingReplicationLag = 0;
+	end
+end)
+
+game.UserInputService.InputBegan:connect(function(input)
+	if input.KeyCode == Enum.KeyCode.Nine then
+		debounce = true	
+	end
+end)
